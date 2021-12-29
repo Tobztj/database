d1292824b95756196be65d890496d646d1f057de
Tobztj <tobijones9@gmail.com>
2021-12-29 17:45:17 +0000

    practice done

---
+++ Sqlite/app.py
@@ -0,0 +1,12 @@
+from sqlite_intro import *
+# import sqlite_intro 
+
+# add_one("Laura", "Smith", "laura@smith.com")
+
+# delete_one('5')
+new_list = [
+            ('Brenda','Smith','brenda@smith.com'),
+            ('Joshua', 'Raintree', 'josh@raintree.com')
+            ]
+add_many(new_list)
+show_all()
--- Sqlite/sqlite_intro.py
+++ Sqlite/sqlite_intro.py
@@ -1,4 +1,5 @@
 import sqlite3
+from sqlite3.dbapi2 import connect
 
 # conn = sqlite3.connect(':memory:') uses the database in memeory but doesnt save it. deletes at 
 # the end of the program. 
@@ -7,10 +8,66 @@ conn = sqlite3.connect('customer.db')
 # create a cursor
 c = conn.cursor()
 
+
+# Query and Return all records from DB
+def show_all():
+    conn = sqlite3.connect('customer.db')
+
+    # create a cursor
+    c = conn.cursor()
+    # Query the database
+    c.execute("SELECT rowid, * FROM customers")
+    items = c.fetchall()
+
+    for item in items:
+        print(item)
+    # Commit commands
+    conn.commit()
+    # Close the connection
+    conn.close()
+
+
+# Add a new record to the table
+def add_one(first, last, email):
+    conn = sqlite3.connect('customer.db')
+
+    # create a cursor
+    c = conn.cursor()
+    c.execute("INSERT INTO customers VALUES (?,?,?)", (first, last, email))
+
+    # Commit commands
+    conn.commit()
+    # Close the connection
+    conn.close()
+
+def add_many(list):
+    c.executemany("INSERT INTO customers VALUES (?,?,?)", (list))
+    conn.commit()
+    conn.close()
+
+#  Delete Record from table
+def delete_one(id):
+    conn = sqlite3.connect('customer.db')
+    c = conn.cursor()
+    c.execute("DELETE FROM customers WHERE rowid = (?)", id)
+    conn.commit()
+    conn.close()
+
+# add_one("Laura", "Smith", "laura@smith.com")
+
+
 #  create a table
 # Datatypes in sqlite NULL, INT, REAL, TEXT, BLOB
+# c.execute("""
+#             CREATE TABLE customers (
+#                 first_name TEXT,
+#                 last_name TEXT,
+#                 email TEXT
+#             )
 
-# Inserting into the Database
+#         """)
+#
+# # Inserting into the Database
 # c.execute("""
 #             INSERT INTO customers VALUES ('John', 'Dow', 'john@doe.com')
 #             """)
@@ -23,26 +80,13 @@ c = conn.cursor()
 # print(c.fetchone()[1])
 # c.fetchmany(3)
 # items = c.fetchall()
+conn = sqlite3.connect('customer.db')
 
-
-# c.execute("""
-#         DELETE FROM customers
-#         WHERE rowid = 6
-#         """)
+# create a cursor
+c = conn.cursor()
 c.execute("""
-            SELECT rowid, * FROM
-            customers
-            LIMIT 2
-            """)
-items = c.fetchall()
+        DELETE FROM customers
+        WHERE rowid IN (5,6,7)
+        """)
 
-for item in items:
-    print(item)
-
-
-# commit our command to the database
 conn.commit()
-# close the connection
-conn.close()
-
-
